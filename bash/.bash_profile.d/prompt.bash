3b1ab17f96d035bd081e61c98a07d22f64512360
Fielding Johnston <fielding@justfielding.com>
2016-05-11 02:20:58 -0500

    Clean up and add new bash prompt

--- bash/.bash_profile.d/prompt.bash
+++ bash/.bash_profile.d/prompt.bash
@@ -1,92 +1,22 @@
-## Define Colors ----------------------------------------------
-
-# Reset
-RS='\[\e[0m\]'       # Text Reset
-
-# Regular Colors
-Black='\[\e[0;30m\]'        # Black
-Red='\[\e[0;31m\]'          # Red
-Green='\[\e[0;32m\]'        # Green
-Yellow='\[\e[0;33m\]'       # Yellow
-Blue='\[\e[0;34m\]'         # Blue
-Purple='\[\e[0;35m\]'       # Purple
-Cyan='\[\e[0;36m\]'         # Cyan
-White='\[\e[0;37m\]'        # White
-
-# Bold
-BBlack='\[\e[1;30m\]'       # Black
-BRed='\[\e[1;31m\]'         # Red
-BGreen='\[\e[1;32m\]'       # Green
-BYellow='\[\e[1;33m\]'      # Yellow
-BBlue='\[\e[1;34m\]'        # Blue
-BPurple='\[\e[1;35m\]'      # Purple
-BCyan='\[\e[1;36m\]'        # Cyan
-BWhite='\[\e[1;37m\]'       # White
-
-# Underline
-UBlack='\e[4;30m'       # Black
-URed='\e[4;31m'         # Red
-UGreen='\e[4;32m'       # Green
-UYellow='\e[4;33m'      # Yellow
-UBlue='\e[4;34m'        # Blue
-UPurple='\e[4;35m'      # Purple
-UCyan='\e[4;36m'        # Cyan
-UWhite='\e[4;37m'       # White
-
-# Background
-On_Black='\e[40m'       # Black
-On_Red='\e[41m'         # Red
-On_Green='\e[42m'       # Green
-On_Yellow='\e[43m'      # Yellow
-On_Blue='\e[44m'        # Blue
-On_Purple='\e[45m'      # Purple
-On_Cyan='\e[46m'        # Cyan
-On_White='\e[47m'       # White
-
-# High Intensty
-IBlack='\e[0;90m'       # Black
-IRed='\e[0;91m'         # Red
-IGreen='\e[0;92m'       # Green
-IYellow='\e[0;93m'      # Yellow
-IBlue='\e[0;94m'        # Blue
-IPurple='\e[0;95m'      # Purple
-ICyan='\e[0;96m'        # Cyan
-IWhite='\e[0;97m'       # White
-
-# Bold High Intensty
-BIBlack='\e[1;90m'      # Black
-BIRed='\e[1;91m'        # Red
-BIGreen='\e[1;92m'      # Green
-BIYellow='\e[1;93m'     # Yellow
-BIBlue='\e[1;94m'       # Blue
-BIPurple='\e[1;95m'     # Purple
-BICyan='\e[1;96m'       # Cyan
-BIWhite='\e[1;97m'      # White
-
-# High Intensty backgrounds
-On_IBlack='\e[0;100m'   # Black
-On_IRed='\e[0;101m'     # Red
-On_IGreen='\e[0;102m'   # Green
-On_IYellow='\e[0;103m'  # Yellow
-On_IBlue='\e[0;104m'    # Blue
-On_IPurple='\e[10;95m'  # Purple
-On_ICyan='\e[0;106m'    # Cyan
-On_IWhite='\e[0;107m'   # White
-
-## Git Functions
 function parse_git_dirty() {
-  [[ $(git status 2> /dev/null | tail -n1) != "nothing to commit (working directory clean)" ]] && echo "*"
+  [[ $(git status 2> /dev/null | tail -n1) != "nothing to commit, working directory clean" ]] && echo "*"
 }
 
 function parse_git_branch() {
   git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/\1$(parse_git_dirty)/"
 }
 
-if [ `id -u` = "0" ]; then
-  PS1="${White}┌─${BWhite}[${Red}\u$${BWhite}]${White}──${BWhite}[${Blue}${HOSTNAME}${BWhite}:${Green} \w${BWhite}]\n${White}└──${Purple}>>${RS}"
-  export PS1;
-  return
-fi
+function git_status() {
+  if [ $(git rev-parse --is-inside-work-tree &>/dev/null; echo "${?}") == '0' ]; then
+    echo "$(tput setaf 3)╺─╸$(tput bold)$(tput setaf 7)[$(tput sgr0)$(tput setaf 7)$(parse_git_branch)$(tput setaf 7)$(tput bold)]"
+  fi
+}
 
-PS1="${White}┌─${BWhite}[${Purple}\u${BWhite}]${RS}${White}──${BWhite}[${Blue}${HOSTNAME}${BWhite}:${Yellow} \w${BWhite}]\$([[ -n \$(git branch 2> /dev/null) ]] && echo \"${White}──${BWhite}[${Red}\")\$(parse_git_branch)${BWhite}\$([[ -n \$(git branch 2> /dev/null) ]] && echo \"${BWhite}]\")\n${White}└──${Purple}>>${RS}"
+
+export PS1="\[$(tput setaf 3)\]┌─╼ \[$(tput bold)$(tput setaf 7)\][\[$(tput sgr0)\]\[$(tput setaf 7)\]\w\[$(tput bold)$(tput setaf 7)\]]\[$(tput sgr0)\]\$(git_status)\n\[$(tput sgr0)$(tput setaf 3)\]\$(if [[ \$? == 0 ]]; then echo \"\[$(tput setaf 3)\]└────╼\"; else echo \"\[$(tput setaf 3)\]└╼\"; fi) \[$(tput setaf 7)$(tput sgr0)\]"
+
 export PS1;
+
+
+
+
