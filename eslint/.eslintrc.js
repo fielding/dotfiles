45d6147c64b45f9cb8d715ab5a82365cac81211d
Fielding Johnston <fielding@justfielding.com>
2017-09-02 06:35:47 -0500

    Allow jsx in .js files for eslint globally via .eslintrc.js

--- eslint/.eslintrc.js
+++ eslint/.eslintrc.js
@@ -1,5 +1,8 @@
 module.exports = {
   extends: 'airbnb',
   env: { browser: true, node: true },
-  rules: { 'arrow-parens': [2, 'as-needed'], 'no-console': 0 },
+  rules: {
+    'arrow-parens': [2, 'as-needed'],
+    'no-console': 0 },
+    'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx'] }]
 };
