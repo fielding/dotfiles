5aef247ffe05c49013a17924e34715712092e5b9
Fielding Johnston <fielding@justfielding.com>
2018-05-10 08:05:23 -0500

    Update global eslint configuration:

--- eslint/.eslintrc.js
+++ eslint/.eslintrc.js
@@ -7,5 +7,14 @@ module.exports = {
     'no-console': 0,
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.mjs'] }],
     'space-in-parens': 0,
+    'no-unused-expressions': ['error', { allowShortCircuit: true, allowTernary: true }],
+    'react/prop-types': 0,
+    'comma-dangle': ['error', {
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+      imports: 'ignore',
+      exports: 'ignore',
+      functions: 'ignore',
+    }],
   },
 };
